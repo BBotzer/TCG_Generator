eb064e22c07e075aff37fdcca1f0afd723411f6d
Etienne Thompson <et@etiennethompson.com>
2025-02-01 17:51:52 -0700

    Support multiple regex per theme.

--- tcg_generator/tcg_generator/theme_parser.py
+++ tcg_generator/tcg_generator/theme_parser.py
@@ -1,6 +1,7 @@
 import json
 import re
 from pathlib import Path
+from typing import Any
 
 from loguru import logger
 
@@ -8,20 +9,71 @@ from tcg_generator.progress_tracker import track_progress
 from tcg_generator.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 THEME_REGEX_MAP = {
-    "Flying": r"Flying"
+    "Activated Abilities": [r"activated abilities"],
+    "Adapt": [r"adapt"],
+    "Affinity": [r"affinity"],
+    "Alternative Cost": [r"may pay (?:.*) rather than pay"],
+    "Amass": [r"amass"],
+    "Any Number Of": [r"a deck can have any number of"],
+    "Aura": [r"(?<!non-)aura"],
+    "Cascade": [r"cascade"],
+    "Combat Damage": [r"combat damage"],
+    "Counter Theme": [r"\b(?:[a-z]+)\bcounter"], # not yet working.
+    "Craft": [r"\bcraft"],
+    "Curse": [r"curse\b"],
+    "Cycling": [r"cycl(?:e|es|ing)"],
+    "Damage": [r"deals (.*)(?<!combat )damage"],
+    "Deathtouch": [r"deathtouch"],
+    "Decayed": [r"decayed"],
+    "Defender": [r"defender"],
+    "Desert": [r"\bdesert\b"],
+    "Devotion": [r"devotion"],
+    "Discover": [r"\bdiscover\b"],
+    "Dredge": [r"\bdredge\b"],
+    "Dungeon": [r"venture into the dungeon", r"complete a dungeon"],
+    "Double Strike": [r"double strike"],
+    "Energy": [r"{e}"],
+    "Enrage": [r"enrage", r"whenever (.*) is dealt damage"],
+    "Equipment": [r"(?<!non-)equip(?:ment)?"],
+    "Escape": [r"escape—", r"gains (?:\")escape", r"has escape"],
+    "Evoke": [r"\bevoke\b"],
+    "Exalted": [r"\bexalted\b", r"attacks alone"],
+    "Exchange": [r"\bexchange\b"],
+    "First Strike": [r"first strike"],
+    "Flash": [r"flash"],
+    "Food": [r"food"],
+    "Flying": [r"flying"],
+    "Gain Control": [r"gain(?:s)? control"],
+    "Haste": [r"haste"],
+    "Lifelink": [r"lifelink"],
+    "Menace": [r"menace"],
+    "Reach": [r"reach"],
+    "Plot": [r"plot(?! counter)"],
+    "Populate": [r"populate"],
+    "Proliferate": [r"proliferate"],
+    "Scry": [r"scry"],
+    "Trample": [r"trample"],
+    "Vigilance": [r"vigilance"],
+    "Ward": [r"ward"],
+    "+1/+1 Counters": [r"\+1/\+1 counter"],
+    "-1/-1 Counters": [r"\-1/\-1 counter"],
 }
 
 PARSED_CARD_THEMES: dict[str, set[str]] = dict()
 
-def parse_dataset(entry):
+def parse_dataset(entry: Any):
     card_name = entry.get("name", "")
-    oracle_text = entry.get("oracle_text", "")
+    oracle_text = entry.get("oracle_text", "").lower()
 
-    for [theme, regex] in THEME_REGEX_MAP.items():
-        match = re.search(regex, oracle_text)
-        if match is not None:
-            PARSED_CARD_THEMES.setdefault(card_name, set())
-            PARSED_CARD_THEMES[card_name].add(theme)
+    reminder_text = r"\(.*\)"
+    oracle_text = re.sub(reminder_text, "", oracle_text)
+
+    for [theme, regexes] in THEME_REGEX_MAP.items():
+        for regex in regexes:
+            match = re.search(regex, oracle_text)
+            if match is not None:
+                PARSED_CARD_THEMES.setdefault(card_name, set())
+                PARSED_CARD_THEMES[card_name].add(theme)
 
 def main(
     input_path: Path = RAW_DATA_DIR / "oracle-cards-20250127220801.json",
