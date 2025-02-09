250ba46a0d7753eefabf9eda36e60b7f0d0b4139
Etienne Thompson <et@etiennethompson.com>
2025-02-08 18:39:30 -0700

    Add more themes to parser.

--- tcg_generator/tcg_generator/theme_parser.py
+++ tcg_generator/tcg_generator/theme_parser.py
@@ -10,15 +10,55 @@ from tcg_generator.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 THEME_REGEX_MAP = {
     "Activated Abilities": [r"activated abilities"],
+    "Adamant": [r"adamant"],
     "Adapt": [r"adapt"],
+    "Additional Combat": [r"additional combat"],
     "Affinity": [r"affinity"],
-    "Alternative Cost": [r"may pay (?:.*) rather than pay"],
+    "Afterlife": [r"afterlife"],
+    "Alliance": [r"alliance —"],
+    "Alternative Cost": [r"may (?:.*) rather than pay"],
     "Amass": [r"amass"],
+    "Annihilator": [r"annihilator"],
     "Any Number Of": [r"a deck can have any number of"],
-    "Aura": [r"(?<!non-)aura"],
+    "Ascend": [r"ascend\b", r"you have the city's blessing"],
+    "Aura": [r"(?<!non-)aura", r"enchant\b", r"bestow\b"],
+    "Awaken": [r"awaken (?:[0-9]+)"],
+    "Backup": [r"backup (?:[0-9]+)"],
+    "Banding": [r"banding\b", r"band(?:s)?\b"],
+    "Bargain": [r"bargain\s+\n"],
+    "Battalion": [r"battalion —", r"and at least two other creatures attack"],
+    "Battle Cry": [r"battle cry (?!goblin)"],
+    "Bestow": [r"bestow\b"],
+    "Blitz": [r"blitz {", r"has blitz"],
+    "Bloodthirst": [r"bloodthirst [0-9x]+"],
+    "Boast": [r"boast\b"],
+    "Bolster": [r"bolster [0-9x]+"],
+    "Bushido": [r"bushido [0-9x]+"],
+    "Buyback": [r"buyback(?:—| {)"],
     "Cascade": [r"cascade"],
+    "Casualty": [r"casualty"],
+    "Celebration": [r"celebration —"],
+    "Champion": [r"champion a(?:n)?\b"],
+    "Changeling": [r"changeling"],
+    "Channel": [r"channel —"],
+    "Choose a Background": [r"choose a background"],
+    "Chroma": [r"chroma —"],
+    "Cipher": [r"\bcipher"],
+    "Clash": [r"\bclash\b"],
+    "Cleave": [r"cleave {"],
+    "Cloak": [r"cloak(?:s)? the top card", r"cloak those cards", r"cloak a card", r"cloak two"],
+    "Clone": [r"enter(?:s)? (tapped )?as a copy"],
+    "Cohort": [r"cohort —"],
+    "Collect Evidence": [r"collect evidence [0-9]+"],
     "Combat Damage": [r"combat damage"],
-    "Counter Theme": [r"\b([a-z]+) counter"], # not yet working.
+    "Commit a Crime": [r"commit a crime"],
+    "Companion": [r"companion —"],
+    "Compleated": [r"compleated\s+\n"],
+    "Connive": [r"connive"],
+    "Conspire": [r"conspire"],
+    "Constellation": [r"constellation —"],
+    "Convoke": [r"convoke"],
+    "Counter Theme": [r"\b([a-z]+) counter(?:s)?\b"], # not yet working.
     "Craft": [r"\bcraft"],
     "Curse": [r"curse\b"],
     "Cycling": [r"cycl(?:e|es|ing)"],
@@ -63,8 +103,10 @@ THEME_REGEX_MAP = {
     "Proliferate": [r"proliferate"],
     "Scry": [r"scry"],
     "Trample": [r"trample"],
+    "Unblockable": [r"can't be blocked"],
+    "Uncounterable": [r"can't be countered"],
     "Vigilance": [r"vigilance"],
-    "Ward": [r"ward"],
+    "Ward": [r"ward(?:—| {)"],
     "+1/+1 Counters": [r"\+1/\+1 counter"],
     "-1/-1 Counters": [r"\-1/\-1 counter"],
 }
@@ -98,6 +140,7 @@ def main(
     track_progress(dataset_json, parse_dataset)
     card_themes = {card_name: list(themes) for card_name, themes in PARSED_CARD_THEMES.items()}
     print()
+    print(f"Total themes checked: {len(THEME_REGEX_MAP.keys())}")
     print(f"Total cards with themes: {len(card_themes)}")
     with open(output_path, "w") as output_file:
         output_file.write(json.dumps(card_themes))
